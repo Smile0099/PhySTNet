18be5ae1d51b77e77e4dcbff2d87744c8dff749c
Smile0099 <149997329+Smile0099@users.noreply.github.com>
2025-09-19 14:02:31 +0800

    Update main.py

--- PredRNN++/main.py
+++ PredRNN++/main.py
@@ -13,7 +13,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 device = configs.device
 model = PredRNNpp_Model(configs).to(configs.device)
-net = torch.load('checkpoint_70.95734786987305.chk')
+net = torch.load('checkpoint_PredRNN++.chk')
 model.load_state_dict(net['net'])
 model.eval()
 dataloader_eval = DataLoader(dataset_test, batch_size=8, shuffle=False)
