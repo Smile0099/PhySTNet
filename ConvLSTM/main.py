63e4bd12057b1753e5e331743f8193eec0440d53
Smile0099 <149997329+Smile0099@users.noreply.github.com>
2025-09-19 13:56:48 +0800

    Update main.py

--- ConvLSTM/main.py
+++ ConvLSTM/main.py
@@ -14,7 +14,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 device = configs.device
 model = ConvLSTM_Model(configs).to(configs.device)
-net = torch.load('checkpoint_70.56432056427002.chk')
+net = torch.load('checkpoint_ConvLSTM.chk')
 model.load_state_dict(net['net'])
 model.eval()
 dataloader_eval = DataLoader(dataset_test, batch_size=8, shuffle=False)
